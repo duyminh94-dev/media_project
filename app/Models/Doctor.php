f791e1a199bb013c9512b7141b18d9e2c918785a
duyminh94-dev <minhkirito002@gmail.com>
2025-11-04 08:12:40 +0700

    UpLoad commit 4/11

---
+++ app/Models/Doctor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'specialty_id',
+        'city_id',
+        'degree',
+        'bio',
+        'experience_years',
+        'available_days',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function specialty() {
+        return $this->belongsTo(Specialty::class);
+    }
+
+    public function city() {
+        return $this->belongsTo(City::class);
+    }
+
+    public function appointments() {
+        return $this->hasMany(Appointment::class);
+    }
+}
---
+++ database/migrations/2025_11_04_001833_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('title');
+            $table->varchar('slug');
+            $table->text('content');
+            $table->string('image');
+            $table->foreignId('author_id')->constrained(table: 'users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
